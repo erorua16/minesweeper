521596f7e59321f3094981b49a4a8513e7e4713f
aurore bouriot <aurore.bouriot@talan.com>
2023-02-15 18:07:37 +0100

    changed grid id values and made grid cleaner

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -5,25 +5,28 @@ interface GridType {
     difficulty: Difficulty;
 }
 
+interface cellIds {
+    position : string,
+    value : string,
+    revealed : boolean
+}
+
 const Grid : React.FC<GridType> = ({ difficulty } : GridType)  => {
 
     const [gridSize, setGridSize] = React.useState<number | null>();
     const [numBombs, setNumBombs] = React.useState<number | null>();
-    const [bombsIds, setBombsIds] = React.useState<string[]>([])
-    const [flippedIds, setFlippedIds] = React.useState<{ [key: string]: boolean }>({});
-
-    const handleFLip = (key :string) => {
-        setFlippedIds({ ...flippedIds, [key]: true });
-    }
+    const [cellIds, setCellIds] = React.useState<cellIds[]>([]);
 
     React.useEffect(() => {
         setGridState()
     }, [difficulty])
 
-    React.useEffect(() => {
-        setBombs()
-    },[gridSize,numBombs])
-    
+    React.useEffect(()=>{
+        if(gridSize && numBombs){
+            setGrid()
+        }
+    }, [gridSize])
+
     const setGridState = () => {
         switch (difficulty) {
           case Difficulty.easy:
@@ -49,61 +52,89 @@ const Grid : React.FC<GridType> = ({ difficulty } : GridType)  => {
         }
       }
 
-      const setBombs = () => {
-       if(gridSize && numBombs){
-        let numBombsPlaced = 0;
-        let bombCellIds: string[] = [];
-        while(numBombsPlaced < numBombs){
+    const handleFlip = (cellId :string) => {
+        var cell = cellIds.find(item => item.position === cellId);
+        console.log(cellIds)
+        if(cell?.revealed){
+            return
+        }
+
+        if(cell?.value === "bomb"){
+            console.log('boom boom')
+        }
+        setCellIds(cellIds =>
+            cellIds.map(cell => {
+                if (cell.position === cellId) {
+                return { ...cell, revealed: true };
+                }
+                return cell;
+            })
+        );
+    }
+
+    const setBombs = () => {
+        if (gridSize && numBombs) {
+            let numBombsPlaced = 0;
+            let bombCellIds: string[] = [];
+        
+            while (numBombsPlaced < numBombs) {
             let randomRow = Math.floor(Math.random() * gridSize);
             let randomCol = Math.floor(Math.random() * gridSize);
             let cellId = `${randomRow}-${randomCol}`;
-            if(!bombCellIds.includes(cellId)){
+            if (!bombCellIds.includes(cellId)) {
                 bombCellIds.push(cellId);
                 numBombsPlaced++;
             }
+            }
+            return bombCellIds;
         }
-        setBombsIds(bombCellIds);
-       }
-      }
-      
-      const onClick = (cellId:string) => {
-        handleFLip(cellId)
-        if (bombsIds.includes(cellId)){
-            //@TODO
-            //Add  gameover logic
-            console.log('boom')
-        }
-        else{
-            //@TODO
-            //Add flip grid functions
-            console.log('hi')
-        }
-      }
+    }
 
-      const renderGrid = () => {
+    const setGrid = () => {
+        if(gridSize && numBombs){
+            let cellIds = [];
+            let bombCellIds = setBombs();
+            for (let i = 0; i < gridSize; i++) {
+                for (let j = 0; j < gridSize; j++) {
+                    let cellId = `${i}-${j}`;
+                    let count = 0
+                    for (let x = -1; x <= 1; x++) {
+                        for (let y = -1; y <= 1; y++) {
+                          if (bombCellIds!.includes(`${i + x}-${j + y}`)) count++;
+                        }
+                    }
+                    if(bombCellIds!.includes(cellId)){
+                        cellIds.push({position : cellId, value: "bomb", revealed : false});
+                    }else{
+                        cellIds.push({position : cellId, value: `${count}`, revealed : false});
+                    }
+                    
+                }
+            }
+            setCellIds(cellIds);
+        }
+    }
+      
+    const renderGrid = () => {
         let grid = [];
         const cellClassColor = " bg-indigo-700"
         var cellClassEmpty = "grid-cell flex flex-col h-10 w-10 border border-gray-400 rounded-md justify-center items-center"
         if(gridSize && numBombs){
-            for (let i = 0; i < gridSize; i++) {
-                let row = [];
-                for (let j = 0; j < gridSize; j++) {
+            for(let i = 0; i < cellIds.length; i ++){
+                let row = []
+                for(let j = 0 ; j < cellIds.length; j++){
                     let cellId = `${i}-${j}`;
-                    let count = 0;
-                    for (let x = -1; x <= 1; x++) {
-                        for (let y = -1; y <= 1; y++) {
-                            if (bombsIds.includes(`${i + x}-${j + y}`)) count++;
+                    var cell = cellIds.find(item => item.position === cellId);
+                    if(cell && cell.position){
+                        if(cell.value === "bomb"){
+                            row.push(<div key={cell.position} className={cell.revealed ? cellClassEmpty : cellClassEmpty + cellClassColor} onClick={() => handleFlip(cellId)}>{cell.revealed ? <i className="fa-solid fa-bomb"></i> : null}</div>);
+                        } else {
+                            row.push(<div key={cell.position} className={cell.revealed ? cellClassEmpty : cellClassEmpty + cellClassColor} onClick={() => handleFlip(cellId)}>{parseInt(cell.value) > 0 && cell.revealed ? cell.value : null}</div>);
                         }
-                    }
-                    if(bombsIds.includes(cellId)){
-                        row.push(<div key={`${i}-${j}`} className={flippedIds[cellId] ? cellClassEmpty : cellClassEmpty + cellClassColor} onClick={() => onClick(cellId)}>{flippedIds[cellId] ? <i className="fa-solid fa-bomb"></i> : null}</div>);
-                    } else {
-                        row.push(<div key={`${i}-${j}`} className={flippedIds[cellId] ? cellClassEmpty : cellClassEmpty + cellClassColor} onClick={() => onClick(cellId)}>{count > 0 && flippedIds[cellId]? count : ''}</div>);
                     }
                 }
                 grid.push(<div key={i} className="grid-row flex flex-row">{row}</div>)
             }
-            
             return grid;
         }
     };
