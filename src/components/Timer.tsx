a5cb1b7fa70f0024fddd4c38e542186ea4e6f03e
aurore bouriot <aurore.bouriot@talan.com>
2023-03-10 12:54:45 +0100

    fixed for replay

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -5,44 +5,45 @@ interface TimerType {
   finalTime?: (a: number, b: number) => void;
 }
 
-const Timer = ({ finalTime,gameState}:TimerType) => {
-  const [minutes, setMinutes] = React.useState(0);
-  const [seconds, setSeconds] = React.useState(0);
+const Timer = ({ finalTime, gameState }: TimerType) => {
+  const [time, setTime] = React.useState(0);
 
   React.useEffect(() => {
-    const interval = setInterval(() => {
-      if (gameState !== GameState.win && gameState !== GameState.lose) {
-        setSeconds(seconds => seconds + 1);
-        if (seconds === 59) {
-          setMinutes(minutes => minutes + 1);
-          setSeconds(0);
-        }
-      }
-    }, 1000);
-    
-    return () => clearInterval(interval);
-  }, [minutes, seconds]);
-
-  React.useEffect(()=> {
-    if (finalTime) finalTime(seconds, minutes);
-    if(gameState === GameState.win || gameState === GameState.lose){
-      setMinutes(0)
-      setSeconds(0)
+    let interval: NodeJS.Timeout;
+    if (gameState === GameState.playing) {
+      interval = setInterval(() => {
+        setTime(time => time + 1);
+      }, 1000);
     }
-  }, [gameState])
+    return () => clearInterval(interval);
+  }, [gameState]);
 
-  return <>
-    <div className="w-24 mx-1 p-2 bg-white text-indigo-700 rounded-lg">
-        <div className="font-mono leading-none" x-text="minutes">{minutes}</div>
+  React.useEffect(() => {
+    if (finalTime) finalTime(Math.floor(time / 60), time % 60);
+  }, [finalTime, time]);
+
+  React.useEffect(() => {
+    if (gameState === GameState.win || gameState === GameState.lose) {
+      setTime(0);
+    }
+  }, [gameState]);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  return (
+    <>
+      <div className="w-24 mx-1 p-2 bg-white text-indigo-700 rounded-lg">
+        <div className="font-mono leading-none">{minutes}</div>
         <div className="font-mono uppercase text-sm leading-none">Minutes</div>
-    </div>
+      </div>
 
-    <div className="w-24 mx-1 p-2 bg-white text-indigo-700 rounded-lg">
-        <div className="font-mono leading-none" x-text="seconds">{seconds}</div>
+      <div className="w-24 mx-1 p-2 bg-white text-indigo-700 rounded-lg">
+        <div className="font-mono leading-none">{seconds}</div>
         <div className="font-mono uppercase text-sm leading-none">Seconds</div>
-    </div>
-  </>
+      </div>
+    </>
+  );
 };
 
 export default Timer;
-
